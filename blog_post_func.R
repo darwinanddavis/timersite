64b032426a786bd48411f77a57924cd65b949f12
darwinanddavis <matthew.malishev@gmail.com>
2019-12-08 00:22:02 -0500

    images/blog/pergamino.jpg

--- blog_post_func.R
+++ blog_post_func.R
@@ -4,7 +4,7 @@ blog_post_func <- function(blogtitle){
   blog_title <- blogtitle
   folder_name <- paste0("blog/",gsub(" ","-",blogtitle) ,".Rmd")
   author <- "Matt Malishev"
-  blog_dir <- paste0("static/images/blog/",gsub(" ","-",blogtitle),"/")
+  blog_dir <- paste0("static/images/blog/",gsub(" ","_",blogtitle),"/")
   dir.create(blog_dir)
   img_file <- paste0(blog_dir,img)
   return(c(blog_title,folder_name,author,img_file))
