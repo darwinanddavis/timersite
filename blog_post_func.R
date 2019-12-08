2514b96338c0d0f07f08a8e083c0f46abbdebcdc
darwinanddavis <matthew.malishev@gmail.com>
2019-12-07 22:09:15 -0500

    img yaml test

--- blog_post_func.R
+++ blog_post_func.R
@@ -4,7 +4,10 @@ blog_post_func <- function(blogtitle){
   blog_title <- blogtitle
   folder_name <- paste0("blog/",gsub(" ","-",blogtitle) ,".Rmd")
   author <- "Matt Malishev"
-  img_file <- paste0("blog/",gsub(" ","-",blogtitle),"_files/",img)
+  blog_dir <- paste0("static/images/blog/",gsub(" ","-",blogtitle),"/")
+  dir.create(blog_dir)
+  img_file <- paste0(blog_dir,img)
   return(c(blog_title,folder_name,author,img_file))
 }
+
 
